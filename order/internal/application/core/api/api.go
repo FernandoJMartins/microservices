686be4db6593e6c42e5e946a824279c275e045ec
Fernando <julio8martins@gmail.com>
2026-06-19 13:19:55 -0300

    feat: order with more than 50 items is not allowed and order with 0 items is not allowed

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -3,6 +3,8 @@ package api
 import (
 	"github.com/FernandoJMartins/microservices/order/internal/application/core/domain"
 	"github.com/FernandoJMartins/microservices/order/internal/ports"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -15,6 +17,21 @@ func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
 }
 
 func (a *Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+
+	var totalItems int32
+
+	for _, item := range order.OrderItems {
+		totalItems += item.Quantity
+	}
+
+	if totalItems > 50 {
+		return domain.Order{}, status.Errorf(codes.InvalidArgument, "Order with more than 50 items is not allowed.")
+	}
+
+	if totalItems == 0 {
+		return domain.Order{}, status.Errorf(codes.InvalidArgument, "Order cannot have zero items.")
+	}
+
 	err := a.db.Save(&order)
 
 	if err != nil {
