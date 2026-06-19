257a82c507f1e1cad57006d8c6e3feb7c2d6c7e3
Fernando <julio8martins@gmail.com>
2026-06-19 14:57:32 -0300

    feat: comment to make essay easier

--- payment/internal/application/core/api/api.go
+++ payment/internal/application/core/api/api.go
@@ -21,6 +21,7 @@ func NewApplication(db ports.DBPort) *Application {
 
 func (a Application) Charge(ctx context.Context, payment domain.Payment) (domain.Payment, error) {
 
+	// time.Sleep(3 * time.Second) // Simula um processamento demorado
 	if payment.TotalPrice > 1000 {
 		return domain.Payment{}, status.Errorf(codes.InvalidArgument, "Payment over 1000 is not allowed")
 	}
